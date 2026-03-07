5315698bab7c71ad0c9ee9393e3e60915157dfba
Rendi Hardiartama <rendirenhard@gmail.com>
2026-03-07 14:43:31 +0700

    fix bug

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -8,15 +8,11 @@ use App\Http\Requests\EventUpdateRequest;
 use App\Http\Resources\EventResource;
 use App\Http\Resources\PaginateResource;
 use App\Interfaces\EventRepositoryInterface;
-use Illuminate\Container\Attributes\Log;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controllers\HasMiddleware;
 use Illuminate\Routing\Controllers\Middleware;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log as FacadesLog;
 use Spatie\Permission\Middleware\PermissionMiddleware;
-
-use function Illuminate\Log\log;
 
 class EventController extends Controller implements HasMiddleware
 {
@@ -26,30 +22,32 @@ class EventController extends Controller implements HasMiddleware
     {
         $this->eventRepository = $eventRepository;
     }
+
     /**
      * Display a listing of the resource.
      */
     public static function middleware()
     {
         return [
-            new Middleware(PermissionMiddleware::using(['event-menu|event-list|event-create|event-edit|event-delete']),only: ['index','getAllPaginated' ,'store', 'show', 'update', 'destroy']),
-            new Middleware(PermissionMiddleware::using(['event-create']),only: ['store']),
-            new Middleware(PermissionMiddleware::using(['event-edit']),only: ['update']),
-            new Middleware(PermissionMiddleware::using(['event-delete']),only: ['destroy']),
+            new Middleware(PermissionMiddleware::using(['event-menu|event-list|event-create|event-edit|event-delete']), only: ['index', 'getAllPaginated', 'store', 'show', 'update', 'destroy']),
+            new Middleware(PermissionMiddleware::using(['event-create']), only: ['store']),
+            new Middleware(PermissionMiddleware::using(['event-edit']), only: ['update']),
+            new Middleware(PermissionMiddleware::using(['event-delete']), only: ['destroy']),
         ];
     }
 
     public function index(Request $request)
     {
-        try{
+        try {
             $Events = $this->eventRepository->getAll(
                 $request->search,
                 $request->limit,
                 true
             );
+
             return ResponseHelper::jsonResponse(true, 'success', EventResource::collection($Events), 200);
-        
-        }catch (\Exception $e) {
+
+        } catch (\Exception $e) {
             return ResponseHelper::jsonResponse(false, $e->getMessage(), null, 500);
         }
     }
@@ -59,17 +57,18 @@ class EventController extends Controller implements HasMiddleware
         $request = $request->validate(
             [
                 'search' => 'nullable|string',
-                'rowPerPage' => 'required|integer'
+                'rowPerPage' => 'required|integer',
             ]
         );
-        try{
+        try {
             $Events = $this->eventRepository->getAllPaginate(
                 $request['search'] ?? null,
                 $request['rowPerPage']
             );
+
             return ResponseHelper::jsonResponse(true, 'success', PaginateResource::make($Events, EventResource::class), 200);
-        
-        }catch (\Exception $e) {
+
+        } catch (\Exception $e) {
             return ResponseHelper::jsonResponse(false, $e->getMessage(), null, 500);
         }
     }
@@ -79,13 +78,11 @@ class EventController extends Controller implements HasMiddleware
      */
     public function store(EventStoreRequest $request)
     {
-        DB::beginTransaction();
         try {
             $event = $this->eventRepository->create($request->all());
-            DB::commit();
+
             return ResponseHelper::jsonResponse(true, 'success', EventResource::make($event), 200);
         } catch (\Exception $e) {
-            DB::rollBack();
             return ResponseHelper::jsonResponse(false, $e->getMessage(), null, 500);
         }
     }
@@ -97,6 +94,7 @@ class EventController extends Controller implements HasMiddleware
     {
         try {
             $event = $this->eventRepository->getById($id);
+
             return ResponseHelper::jsonResponse(true, 'success', EventResource::make($event), 200);
         } catch (\Exception $e) {
             return ResponseHelper::jsonResponse(false, $e->getMessage(), null, 500);
@@ -110,10 +108,13 @@ class EventController extends Controller implements HasMiddleware
     {
         $request = $request->validated();
         try {
-            $Events=$this->eventRepository->getById($id);
+            $Events = $this->eventRepository->getById($id);
             FacadesLog::info($Events);
-            if(!$Events) return ResponseHelper::jsonResponse(false, 'event not found', null, 404);
+            if (! $Events) {
+                return ResponseHelper::jsonResponse(false, 'event not found', null, 404);
+            }
             $event = $this->eventRepository->update($id, $request);
+
             return ResponseHelper::jsonResponse(true, 'success', EventResource::make($event), 200);
         } catch (\Exception $e) {
             return ResponseHelper::jsonResponse(false, $e->getMessage(), null, 500);
@@ -126,9 +127,12 @@ class EventController extends Controller implements HasMiddleware
     public function destroy(string $id)
     {
         try {
-           $events = $this->eventRepository->getById($id);
-           if(!$events) return ResponseHelper::jsonResponse(false, 'event not found', null, 404);
+            $events = $this->eventRepository->getById($id);
+            if (! $events) {
+                return ResponseHelper::jsonResponse(false, 'event not found', null, 404);
+            }
             $this->eventRepository->delete($id);
+
             return ResponseHelper::jsonResponse(true, 'success', null, 200);
         } catch (\Exception $e) {
             return ResponseHelper::jsonResponse(false, $e->getMessage(), null, 500);
